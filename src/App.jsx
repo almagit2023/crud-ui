3ba53b0c7ed788f6a7fe7c1c4cccd1ff1ed5f2a4
almacode1703 <testgitt.2023@gmail.com>
2025-06-04 09:07:44 +0530

    Added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,25 @@ import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Contact from './pages/contact/Contact'
 import UpdateRecords from './components/updateRecords/UpdateRecords'
+import LogIn from './pages/login/LogIn'
+import SignUp from './pages/signup/SignUp'
 
 export default function App() {
   return (
     <div>
       <Header/>
       <Routes>
-        <Route path="/" element={<Navigate to='/home'/>}/>
+        <Route path="/" element={<Navigate to='/login'/>}/>
+
+        <Route path='/login' element={<LogIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
         <Route path="/home" element={< Home/>}/>
+
         <Route path="/create" element={< Create/>}/>
-        <Route path="/contact" element={< Contact/>}/>
         <Route path='/:id' element={<UpdateRecords/>} />
+        
+        <Route path="/contact" element={< Contact/>}/>
+
       </Routes>
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import DisplayRecords from '../../components/displayRecords/DisplayRecords'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
   return (
