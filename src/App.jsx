f5b18025cfbf3dc69cb1502fd6a4b3cb648df4ac
almacode1703 <testgitt.2023@gmail.com>
2025-06-04 09:49:26 +0530

    Added Protected/ Private routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,31 @@ import Contact from './pages/contact/Contact'
 import UpdateRecords from './components/updateRecords/UpdateRecords'
 import LogIn from './pages/login/LogIn'
 import SignUp from './pages/signup/SignUp'
+import { useState } from 'react'
+import RefreshHandler from './components/refreshHandler/RefreshHandler'
+
 
 export default function App() {
+  const [isAuthenticated, setIsAuthnticated] = useState(false);
+
+  const PrivateRoute = ({element})=>{
+     return isAuthenticated ? element : <Navigate to="/login"/>
+  }
+
   return (
     <div>
       <Header/>
+      <RefreshHandler setAuthenticated={setIsAuthnticated}/>
       <Routes>
         <Route path="/" element={<Navigate to='/login'/>}/>
 
         <Route path='/login' element={<LogIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
-        <Route path="/home" element={< Home/>}/>
+        <Route path="/home" element={< PrivateRoute element={<Home/>}/>}/>
 
         <Route path="/create" element={< Create/>}/>
         <Route path='/:id' element={<UpdateRecords/>} />
-        
+
         <Route path="/contact" element={< Contact/>}/>
 
       </Routes>
